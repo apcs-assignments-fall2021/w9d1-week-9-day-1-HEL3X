6a06d8fd07331b8bb460fc166715d0cdd216ffc0
HEL3X <person_of_paradise@hotmail.com>
2021-10-28 22:19:52 -0700

    .

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,6 +1,26 @@
+import java.util.*;
 public class BankAccount {
     // Instance variables
-    private int balance;
-    private int accountNumber;
-    private String preferredName;
+    private int balance = 1000;
+    private int accountNumber = 1019401124;
+    private String preferredName = " ";
+
+    public void withdraw(int x){
+        this.balance -= x;
+    }
+    public void deposit(int x){
+        this.balance += x;
+    }
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+    public int getBalance(){
+        return balance;
+    }
+    public void setPreferredName(String preferredNameChange){
+        preferredName = preferredNameChange;
+    }
+    public String getPreferredName(){
+        return preferredName;
+    }
 }
